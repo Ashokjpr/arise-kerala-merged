970922eaa26019c8a451ff3b0e238b0de0fca87a
Ashokjpr <meenaashokkumar260@gmail.com>
2026-02-19 12:11:51 +0530

    Update typography and button styles in Hero and Features sections for improved responsiveness and aesthetics

--- app/(pages)/_components/FeaturesSection.tsx
+++ app/(pages)/_components/FeaturesSection.tsx
@@ -18,7 +18,7 @@ const features = [
   return (
     <section className="pt-20 bg-white">
       <div className="text-center mb-12">
-        <h3 className="text-3xl font-bold text-gray-800">
+        <h3 className="text-2xl md:text-3xl font-bold text-gray-800">
           Arise Medical Academy features
         </h3>
         <p className="text-gray-500 mt-4">
--- app/(pages)/_components/Hero.tsx
+++ app/(pages)/_components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section
-      className="relative pt-20  bg-cover bg-center bg-no-repeat"
+      className="relative pt-10 md:pt-20  bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: "url('/images/hero-bg.webp')" }}
     >
       {/* Overlay */}
@@ -12,16 +12,16 @@ export default function Hero() {
 
       {/* Content */}
       <div className="relative max-w-4xl mx-auto px-6 text-center text-white">
-        <h2 className="text-5xl font-bold mb-6" data-aos="fade-left">Arise Medical Academy Jaipur</h2>
-        <p className="text-xl mb-8"data-aos="fade-right">
+        <h2 className="text-2xl md:text-5xl font-bold mb-4 md:mb-6" data-aos="fade-left">Arise Medical Academy Jaipur</h2>
+        <p className="text-sm md:text-xl mb-8"data-aos="fade-right">
           A coaching and training institute focused on medical exam preparation, especially for FMGE/MCI Screening Examination and related exams.
         </p>
 
         <div className="flex justify-center gap-6">
-          <button className="bg-black px-6 py-3 rounded-lg hover:bg-gray-900 transition"data-aos="fade-left">
-            View All Courses
+          <button className="bg-blue-600  px-3 py-2 md:px-6 md:py-3 rounded-lg hover:bg-blue-700 transition"data-aos="fade-left">
+            View Courses
           </button>
-          <button className="bg-white text-blue-600 px-6 py-3 rounded-lg hover:bg-gray-200 transition" data-aos="fade-right">
+          <button className="bg-white text-blue-600  px-3 py-2 md:px-6 md:py-3 rounded-lg hover:bg-gray-200 transition" data-aos="fade-right">
             Contact Us
           </button>
         </div>
@@ -31,7 +31,7 @@ export default function Hero() {
                 alt="Hero Image"
                 width={1200}
                 height={600}
-                className="mx-auto min-w-[80%] "
+                className="mx-auto  md:h-auto min-w-[90%]"
             />
         </div>
       </div>
